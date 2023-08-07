c78b12fa810bc89c779b164c89b4d61de503ff29
KamaTAEWOO <zszs2020@naver.com>
2023-08-07 13:59:58 +0900

    [feat] DataBinding 연결

---
+++ app/src/main/java/com/fast/weatherinfo/presenter/viewmodel/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.fast.weatherinfo.presenter.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewModel : ViewModel() {
+}
