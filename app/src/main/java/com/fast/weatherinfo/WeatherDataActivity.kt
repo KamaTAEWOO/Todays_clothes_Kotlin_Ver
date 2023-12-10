6399c091d9b876812c1701521df8e35ee057b84e
kamamerona <zszs2020@naver.com>
2023-12-10 13:43:59 +0900

    [res] bottom navigation 설계 완료, app bar 추가 완료

--- app/src/main/java/com/fast/weatherinfo/WeatherDataActivity.kt
+++ app/src/main/java/com/fast/weatherinfo/WeatherDataActivity.kt
@@ -1,6 +1,8 @@
 package com.fast.weatherinfo
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.fast.weatherinfo.base.BaseActivity
 import com.fast.weatherinfo.databinding.ActivityWeatherDataBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,5 +18,13 @@ class WeatherDataActivity : BaseActivity<ActivityWeatherDataBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.i("$TAG::날씨 데이터 파싱")
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
