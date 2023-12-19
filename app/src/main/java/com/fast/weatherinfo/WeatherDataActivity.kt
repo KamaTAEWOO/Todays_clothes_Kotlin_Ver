dc485c164c570160628ef436740a67da736ab92e
kamamerona <zszs2020@naver.com>
2023-12-19 22:08:15 +0900

    [feat] weatherDataViewModel 생성

--- app/src/main/java/com/fast/weatherinfo/WeatherDataActivity.kt
+++ app/src/main/java/com/fast/weatherinfo/WeatherDataActivity.kt
@@ -1,16 +1,20 @@
 package com.fast.weatherinfo
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.fast.weatherinfo.base.BaseActivity
 import com.fast.weatherinfo.databinding.ActivityWeatherDataBinding
+import com.fast.weatherinfo.viewmodel.WeatherDataViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
 class WeatherDataActivity : BaseActivity<ActivityWeatherDataBinding>() {
     private val TAG = "WeatherDataActivity::"
+
+    private val weatherDataViewModel: WeatherDataViewModel by viewModels()
 
     override fun getActivityBinding(): ActivityWeatherDataBinding =
         ActivityWeatherDataBinding.inflate(layoutInflater)
@@ -19,7 +23,15 @@ class WeatherDataActivity : BaseActivity<ActivityWeatherDataBinding>() {
         super.onCreate(savedInstanceState)
         Timber.i("$TAG::날씨 데이터 파싱")
 
+        initViewModel()
         initBottomNavigation()
+    }
+
+    private fun initViewModel() {
+        binding.apply {
+            lifecycleOwner = this@WeatherDataActivity
+            viewModel = weatherDataViewModel
+        }
     }
 
     private fun initBottomNavigation() {
---
+++ app/src/main/java/com/fast/weatherinfo/viewmodel/WeatherDataViewModel.java
@@ -0,0 +1,7 @@
+package com.fast.weatherinfo.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class WeatherDataViewModel extends ViewModel {
+
+}
